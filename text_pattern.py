4fe50ac3a30f9b48c831583533950b9162152be4
Karthik <46178762+Karthikzee@users.noreply.github.com>
2019-03-22 12:26:55 +0530

    Add files via upload

---
+++ text_pattern.py
@@ -0,0 +1,8 @@
+#Printing text in pattern
+
+name = input("Enter your name: ")
+branch = input("Enter your branch: ")
+place = input("Enter your place: ")
+print("\t\t\t\tHii Iam\t", name, "\n\t\t\t\t\tIam studying in", branch, "\n\t\t\t\tI live in", place)
+
+
