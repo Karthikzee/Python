71c5dc4fcf648b132ce0e282de6d76c36c9ab45f
Karthik <46178762+Karthikzee@users.noreply.github.com>
2019-04-11 19:07:16 +0530

    Update ploting_csv.py

--- ploting_csv.py
+++ ploting_csv.py
@@ -1,19 +1,26 @@
 
+#plotting a graph using data of a CSV file
 import matplotlib.pyplot as plt
 import csv
-filename = "sheet1.csv"
+
+#file location of the csv file
+filename = "D:\Sheet1.csv"
 
 x = []
 y = []
- 
-with open(filename, 'r') as csvfile: 
-	csvreader = csv.reader(csvfile, delimiter=",")
 
-	for row in csvreader:
-		x.append(int(row[0])) 
-		y.append(int(row[1]))
+#opening csv file to read
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-plt.plot(x, y, color='b', linestyle='--', marker='o', markerfacecolor='g', markersize=5)
+#iterating over the objects in csv file 
+    for row in csvreader:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+#adding color,size and style to marker and line 
+plt.plot(x, y, color='b', linestyle='-', marker='o', markerfacecolor='g', markersize=5)
+
+#naming the axis and the graph
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.title("Plotting from csv data")
